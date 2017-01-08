5f3b48172571bd8bca03bad00d61a022dfa051c1
Sherin Aly <sherin@vt.edu>
2017-01-08 04:40:48 -0500

    first commit

---
+++ Sorter/ArraySorter.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Sorter
+{
+    public class ArraySorter
+    {
+
+        /// <summary>
+        /// Bubble sorting.
+        /// </summary>
+        /// <param name="numbersArrayInts">The numbers array ints.</param>
+        public static void BubbleSort(int[] numbersArrayInts)
+        {
+            var n = numbersArrayInts.Length;
+            bool swaped;
+            do
+            {
+                swaped = false;
+                for (var i = 1; i <= n - 1; i++)
+                {
+                    if (numbersArrayInts[i - 1] <= numbersArrayInts[i]) continue;
+                    Swap(numbersArrayInts, i - 1, i);
+                    swaped = true;
+                }
+            }
+            while (swaped);
+        }
+
+        /// <summary>
+        /// Swaps two numbers in the given array using the specified locations.
+        /// </summary>
+        /// <param name="numbers">The numbers.</param>
+        /// <param name="idx1">The idx1.</param>
+        /// <param name="idx2">The idx2.</param>
+        public static void Swap(int[] numbers, int idx1, int idx2)
+        {
+            var tmp = numbers[idx1];
+            numbers[idx1] = numbers[idx2];
+            numbers[idx2] = tmp;
+        }
+    
+
+       
+    }
+}
---
+++ Sorter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = { 2, 1, 3 };
+            ArraySorter.BubbleSort(numbers);
+            Print(numbers);
+            Console.ReadKey();
+
+            //var list = new List<int> {2, 3, 1, 5};
+
+            //var lengths = from x in list
+            //              orderby x
+            //              select x;
+
+            //foreach (var value in lengths)
+            //{
+            //    Console.WriteLine(value);
+            //}
+            //Console.ReadLine();
+        }
+      
+        /// <summary>
+        /// Prints the specified arr.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        public static void Print(int[] arr)
+        {
+            foreach (var v in arr)
+            {
+                Console.Write(v.ToString() + ",");
+            }
+
+        }
+       
+
+    }
+
+}
